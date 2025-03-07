48e26d72cdc58733a6d7bebf8c94acca119507bf
李燃东 <lirandong@maxrocky.com>
2025-03-07 13:43:50 +0800

    fix

---
+++ dist/src/style/PdfHighlighter.module.css.js
@@ -0,0 +1,14 @@
+const container = "_container_12oj9_1";
+const highlightLayer = "_highlightLayer_12oj9_8";
+const disableSelection = "_disableSelection_12oj9_19";
+const styles = {
+  container,
+  highlightLayer,
+  disableSelection
+};
+export {
+  container,
+  styles as default,
+  disableSelection,
+  highlightLayer
+};
